10109fa55cd1c9dce6c1517ec205b5b2c1e903d5
Dharsini5 <138126680+Dharsini5@users.noreply.github.com>
2024-07-27 19:02:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CSS Styling</title>
+    <!-- external css link -->
+    <link rel="stylesheet" type="text/css" href="Styles.css">
+    <!-- internal css -->
+    <style>
+        h4 {
+            color: royalblue;
+        }
+    </style>
+</head>
+<body>
+    <!-- inline css -->
+    <center>
+        <h3 style="color: purple;">I'm Peacock</h3>
+    </center>
+    <div>
+        <h4>I'm your favourite!!</h4>
+    </div>
+    <details>
+        <summary>Read more about me!...</summary>
+        <b><p>Peacocks, with their iridescent feathers shimmering in hues of blue and green, epitomize nature's artistry.<span> Each feather's eye-like</span> pattern mesmerizes, evoking a sense of wonder and awe. Their regal strut and graceful movements captivate all who behold them. The peacock's plumage, a cascade of vibrant colors, embodies elegance and splendor. Truly, they are the crowned jewels of the avian world.</p></b>
+    </details>
+    <a href="https://www.google.com/search?q=peacock+images&sca_esv=db972627766451b9&sca_upv=1&rlz=1C1GCEA_enIN1068IN1069&udm=2&biw=1536&bih=730&ei=5WpyZsbPIsyu4-EPyNmV8Aw">Explore me</a>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+    </ul>
+</body>
+</html>
